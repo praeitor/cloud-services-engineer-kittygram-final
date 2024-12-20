cf87ffc5fba6c9eac307d64270733f55cdf0e0c1
Bulgakov Denis <bulgakovdenis@KapMac770-M.local>
2024-12-20 18:02:00 +0400

    minor changes 2

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -62,7 +62,7 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB', 'kittygram'),
         'USER': os.getenv('POSTGRES_USER', 'kittygram_user'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'kittygram_password'),
-        'HOST': os.getenv('DB_HOST', 'db'),
+        'HOST': os.getenv('DB_HOST', 'postgres'),
         'PORT': os.getenv('DB_PORT', 5432),
     }
 }
