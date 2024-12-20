842406e66c44610e003870830a137596304b3ea5
Bulgakov Denis <bulgakovdenis@KapMac770-M.local>
2024-12-20 18:39:56 +0400

    some changes if docker compose

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -62,7 +62,7 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB', 'kittygram'),
         'USER': os.getenv('POSTGRES_USER', 'kittygram_user'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'kittygram_password'),
-        'HOST': os.getenv('DB_HOST', 'postgres'),
+        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
         'PORT': os.getenv('DB_PORT', 5432),
     }
 }
