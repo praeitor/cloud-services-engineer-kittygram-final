a2c7abf29fafca01f7fb015259921ba14f6b6db0
Denis Bulgakov <praeitor@gmail.com>
2024-12-21 17:18:33 +0400

    changes in nginx conf

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -106,7 +106,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/app/collected_static'
+STATIC_ROOT = '/app/collected_static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
